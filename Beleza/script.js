8704f11ecd2243e43be22f945f4c8db971603b5f
Francis Ferreira <francis_patricio_@hotmail.com>
2026-02-25 14:43:23 -0300

    Commit final

---
+++ Beleza/script.js
@@ -0,0 +1,5 @@
+window.sr = ScrollReveal({reset:true}); /* Resetando qualquer revelaçãoa anterior */
+
+sr.reveal('.grid-conteudo', {duration: 3000});
+sr.reveal('.video', {duration: 6000});
+sr.reveal('#grid-equipe', {rotate: {x: 0, y: 100, z: 0}, duration: 6000});
